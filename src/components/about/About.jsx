776f3e6fc3fe5b88796ddf051de3c152b0e0ca7d
Danya Baron <dbaron@wpi.edu>
2022-11-14 13:36:08 -0500

    i think we finished? possibly

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,24 +1,35 @@
 import React from 'react'
 import './about.css'
+import Slide from 'react-reveal/Slide'
 
 const about = () => {
     return (
         <section id='about'>
-           
-           <h5>Get To Know</h5>
+
+
+           <Slide left>
+            <h5>Get To Know</h5>
             <h2>About Me</h2>
-            <div className="container about_container">
+           </Slide>
+           
+           <Slide right>
+                <div className="container about_container">
             
               
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Deserunt modi, nam doloribus quibusdam at dicta atque ut 
-                        cupiditate enim maxime corporis inventore perspiciatis s
-                        oluta unde adipisci, facilis velit! Exercitationem, dolore.
-                    </p>
+            <p>
+                I am a driven and ambitious problem solver who likes to tackle problems head-on. My interest
+                in development started when I was in middle school and wanted to create custom Tumblr themes. I have my BS in Computer
+                Science from WPI, and I am a Girls Who Code alumna. I have an interest in front-end as well as full stack development,
+                and have a vast experience of using different tools and technologies. In addition to development, I love to learn about astrology, exercise, cook, dance, and
+                do yoga. 
+            </p>
 
-            
-            </div>
+    
+    </div>
+
+</Slide>
+           
+
         </section>
     )
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,68 @@
 import React from 'react'
+import './portfolio.css'
+import factorIMG from '../../assets/7factor.png'
+import danyaIMG from '../../assets/danya-website.png'
+import Rotate from 'react-reveal/Rotate'
 
 const Portfolio = () => {
     return (
-        <div>
-            Portfolio
-        </div>
+        <section id='portfolio'>
+            <Rotate bottom right>
+            <h2>
+                Projects
+            </h2>
+
+            </Rotate>
+            
+
+        
+            <div className="container portfolio_container">
+                <Rotate top left>
+                <div className="portfolio_item">
+                    <div className="portfolio_item-image">
+                        <img src={factorIMG} alt=''/>                    
+                    </div>
+                    <h3>
+                        7Factor Website
+                    </h3>
+                    <h5>
+                        My senior year project at WPI. Our team created this website using the design mockups we received 
+                        from scratch in ReactJS.
+                    </h5>
+                    <div className="portfolio_item-cta">
+                        <a href="https://www.7factor.io/" target="_blank" className="btn"> Project Website </a>
+                    </div>
+                    
+                </div>
+
+                </Rotate>
+
+                <Rotate top right>
+
+                
+                <div className="portfolio_item">
+                    <div className="portfolio_item-image">
+                        <img src={danyaIMG} alt=''/>                    
+                    </div>
+                    <h3>
+                        Portfolio Website
+                    </h3>
+                    <h5>
+                        My portfolio wesbite built fully in ReactJS.
+                    </h5>
+                        <div className="portfolio_item-cta">
+                            <a href="https://github.com/danyabaron/danyabaron.github.io" target="_blank" className="btn"> Project Github </a>
+                        </div>
+                    </div>
+
+                </Rotate>
+
+
+            </div>
+        
+
+        </section>
+
     )
 }
 
