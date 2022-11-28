191e6c3b4739c5b0f68eb8857d7bc50fd0155206
Sanskar Sahu <sahup3296@gmail.com>
2022-11-28 17:56:26 +0000

    Main fixed all issues

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './App'
 import './index.css'
 
+// ReactDOM.render(<App/>, document.getElementById('root'));
 
+import React, { StrictMode } from 'react';
+import * as ReactDOMClient from 'react-dom/client';
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+import App  from './App';
+
+const rootElement = document.getElementById('root');
+const root = ReactDOMClient.createRoot(rootElement);
+
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
